cb1a7c62a601479275ed8ba2880654b62621b066
Daniel <dmossie96@gmail.com>
2018-04-07 21:20:04 -0700

    Added filtering and previous results

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 import requests, time
 from xml.etree import ElementTree
 from pprint import pprint
+from datetime import datetime, timedelta
 
 # Get the feed data
 def make_request(url):
@@ -33,22 +34,70 @@ def parse_data(data):
 			title = element[0].text
 			price = 0
 
+		# Shift time back by 7 hours
+		raw_time = element[3].text.strip('-0000').strip()
+		parsed_time = datetime.strptime(raw_time, "%a, %d %b %Y %H:%M:%S")
+		pubdate = parsed_time - timedelta(hours=7)
+
 		# Add properties to the new item
 		new_item['title'] = title
 		new_item['price'] = int(price)
 		new_item['link'] = element[1].text
 		#new_item['description'] = element[2].text
-		new_item['pubdate'] = element[3].text
+		new_item['pubdate'] = pubdate
 
 		# Add the new item to the list of all items
 		items.append(new_item)
 
 	return items
 
-url = "http://www.usedvictoria.com/index.rss?category=all"
-keywords = ['table']
+# Search the title for each item, if it has all keywords in it, return it
+def find_matches(items, keywords):
+	matches = []
+	for item in items:
+		for keyword in keywords:
+			if keyword not in item['title']:
+				break
+			if keyword == keywords[-1]:
+				matches.append(item)
+	return matches
 
-content = make_request(url)
-data = parse_data(content)
+# Add items from one list to another, if they don't exist already
+def add_new(main_list, new_list):
+	for i in new_list:
+		if i not in main_list:
+			main_list.append(i)
 
-pprint(data)
+	return main_list
+# User Input
+url = "http://www.usedvictoria.com/index.rss?category=motorcycles"
+keywords = ['2008']
+
+# Current List
+data = []
+matches = []
+
+
+# Program
+while True:
+	# Get and parse feed
+	request = make_request(url)
+	content = parse_data(request)
+
+	# Find out which are the new items
+	new_items = []
+	for i in content:
+		if i not in data:
+			new_items.append(i)
+
+	# Check if new items have key words
+	new_matches = find_matches(new_items, keywords)
+	matches = add_new(matches,new_matches)
+
+	# Do something with the matches
+	print matches
+
+	# Save the new items and sleep
+	data = add_new(data, new_items)
+
+	time.sleep(30)
