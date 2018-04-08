91f1592edc6a1adc197d0b051ee3ee5a690d083d
Daniel <dmossie96@gmail.com>
2018-04-07 17:30:28 -0700

    Formatted

--- parser.py
+++ parser.py
@@ -1,46 +1,54 @@
 import requests, time
 from xml.etree import ElementTree
+from pprint import pprint
 
+# Get the feed data
+def make_request(url):
+	while True:
+		page = requests.get(url)
 
-while True:
-
-	# Load RSS feed
-	page = requests.get("http://www.usedvictoria.com/index.rss?category=all")
+		# Request the page again if used vic had a 503
+		if '503 Service Temporarily Unavailable' not in page.content:
+			break
 	
-	# Request the page again if if used vic had a 503
-	if '503 Service Temporarily Unavailable' not in page.content:
+	return page.content
 
-		# Parse the items
+# Parse the items
+def parse_data(data):
 
-		# Hold all the items here
-		items = []
+	# Hold all the items here
+	items = []
 
-		# For every item
-		tree = ElementTree.fromstring(page.content)
-		for element in tree[0].findall('item'):
+	# For every item
+	tree = ElementTree.fromstring(data)
+	for element in tree[0].findall('item'):
 
-			# Item dictionary
-			new_item = {}
+		# New item dictionary
+		new_item = {}
 
-			# Parse the price and title from the title element
-			if len(element[0].text.split('- $'))  == 2:
-				title,price = element[0].text.split('- $')
-			else:
-				title = element[0].text
-				price = 0
+		# Parse the price and title from the title element
+		if len(element[0].text.split('- $'))  == 2:
+			title,price = element[0].text.split('- $')
+		else:
+			title = element[0].text
+			price = 0
 
-			# Add properties to the new item
-			new_item['title'] = title
-			new_item['price'] = int(price)
-			new_item['link'] = element[1].text
-			new_item['description'] = element[2].text
-			new_item['pubdate'] = element[3].text
+		# Add properties to the new item
+		new_item['title'] = title
+		new_item['price'] = int(price)
+		new_item['link'] = element[1].text
+		#new_item['description'] = element[2].text
+		new_item['pubdate'] = element[3].text
 
-			# Add the new item to the list of all items
-			items.append(new_item)
+		# Add the new item to the list of all items
+		items.append(new_item)
 
-		for item in items:
-			if 'table' in item['title']:
-				print item
+	return items
 
-		time.sleep(10)
+url = "http://www.usedvictoria.com/index.rss?category=all"
+keywords = ['table']
+
+content = make_request(url)
+data = parse_data(content)
+
+pprint(data)
